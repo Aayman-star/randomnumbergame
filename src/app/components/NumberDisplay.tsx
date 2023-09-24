e60a79a2e791eeebc41a7d0cb6396db2558ce0dc
Aayman-star <aayman.khalid1@gmail.com>
2023-09-24 13:58:17 +0500

    adjusted the app

--- src/app/components/NumberDisplay.tsx
+++ src/app/components/NumberDisplay.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
-const NumberDisplay = () => {
-  return <div>The number will be displayed here</div>;
+interface CompProp {
+  RandomNumber: number;
+}
+
+const NumberDisplay = ({ RandomNumber }: CompProp) => {
+  const [localNumber, setLocalNumber] = useState<number>(RandomNumber);
+  const generateNewRandomNumber = () => {
+    const newNumber = Math.floor(Math.random() * 100);
+    setLocalNumber(newNumber);
+  };
+  return (
+    <div className="bg-gradient-to-r from-blue-500  to-purple-500 text-slate-100 p-10 rounded-md shadow-md">
+      <div className="flex flex-col items-center justify-center gap-y-4 md:flex-row md:items-center md:justify-between">
+        <p className="font-light text-2xl">
+          The number Generated is :{" "}
+          <span className="font-semibold text-3xl">{localNumber}</span>
+        </p>
+        <button
+          onClick={generateNewRandomNumber}
+          className="p-3 border-2 border-slate-200 shadow-md rounded-full">
+          New Number
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default NumberDisplay;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 import Image from "next/image";
 import NumberDisplay from "./components/NumberDisplay";
+import Wall6 from "/public/image/Wall6.jpg";
 
 export default function Home() {
+  const randomNumber = Math.floor(Math.random() * 100);
+
+  console.log(randomNumber);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-evenly p-28 bg-stone-300">
-      <h1>Test Random Number Generator</h1>
-      <NumberDisplay />
+    <main className="relative h-screen">
+      <div className="absolute inset-0 ">
+        <Image
+          src={Wall6}
+          alt="bg-image"
+          quality={100}
+          fill
+          sizes="100vw"
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+      <div className="relative z-10 flex flex-col items-center justify-center gap-y-6 h-full">
+        <h1 className="text-2xl md:text-4xl text-slate-100 font-bold">
+          Test Random Number Generator
+        </h1>
+        <div className="w-full md:w-1/2 text-center">
+          {" "}
+          <NumberDisplay RandomNumber={randomNumber} />
+        </div>
+      </div>
     </main>
   );
 }
