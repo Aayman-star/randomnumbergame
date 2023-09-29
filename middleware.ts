f3302e7735026ad480f9050a0405c4bb7826915d
Aayman-star <aayman.khalid1@gmail.com>
2023-09-29 22:44:38 +0500

    tried a new approach to number generation

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export  async function  middleware  (request: NextRequest) {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    // const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   console.log(request);
-  const aNumber = await fetch(`${baseUrl}/api/randomNumber`)
-  return NextResponse.json({aNumber},{status:200})
+  // const aNumber = await fetch(`${baseUrl}/api/randomNumber`)
+  return NextResponse.next()
 }
